5bc8798c7dad7db51cd4856ba9c839063bc65c86
kaffw <coroneljm01@gmail.com>
2024-05-12 11:17:03 +0800

    CLEAN
    clean commit for views.py

--- MerchTrackProject/MerchTrackApp/models.py
+++ MerchTrackProject/MerchTrackApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your database models here.
 
 class userInfo(models.Model):
-    student_id = models.CharField(max_length=9)
+    student_id = models.CharField(max_length=9, validators=[RegexValidator(r'^[0-9]{1,9}$', 'Student ID must be numeric and have at most 9 digits.')])
     student_name = models.CharField(max_length=200)
--- MerchTrackProject/MerchTrackApp/views.py
+++ MerchTrackProject/MerchTrackApp/views.py
@@ -1,23 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import userInfo
 
 def home(request):
     return render(request, "index.html")
-    # return HttpResponse("Welcome to MerchTrack")
 
 def trackOrder(request):
-    #student_id = request.POST.get('student_id')
     student_id = request.GET.get('student_id')
 
-    if student_id:
-        try:
-            # Query only for student_id, not both student_id and student_name
-            student = userInfo.objects.get(student_id=student_id)
-            return render(request, "track_order.html")
-        except userInfo.DoesNotExist:
-            student = None
-            return render(request, "index.html")
-
-    else:
+    try:
+        student = userInfo.objects.get(student_id=student_id)
+        return render(request, "track_order.html")
+    except userInfo.DoesNotExist:
         student = None
-        return render(request, "index.html")
+        return render(request, "index.html", {'error_message': 'student id does not exist'})
