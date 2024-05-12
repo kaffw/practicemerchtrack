d76a87ba9eb3506ef326d0f6dcb47ccfbeddaaa1
kaffw <coroneljm01@gmail.com>
2024-05-12 10:58:01 +0800

    ADD
    userInfo database
    > student_id, student_name
    
    authentication using student_id
    user id found => order tracking page
    
    intial order tracking page

--- MerchTrackProject/MerchTrackApp/models.py
+++ MerchTrackProject/MerchTrackApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your database models here.
+
+class userInfo(models.Model):
+    student_id = models.CharField(max_length=9)
+    student_name = models.CharField(max_length=200)
--- MerchTrackProject/MerchTrackApp/views.py
+++ MerchTrackProject/MerchTrackApp/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from .models import userInfo
 
 def home(request):
     return render(request, "index.html")
     # return HttpResponse("Welcome to MerchTrack")
+
+def trackOrder(request):
+    #student_id = request.POST.get('student_id')
+    student_id = request.GET.get('student_id')
+
+    if student_id:
+        try:
+            # Query only for student_id, not both student_id and student_name
+            student = userInfo.objects.get(student_id=student_id)
+            return render(request, "track_order.html")
+        except userInfo.DoesNotExist:
+            student = None
+            return render(request, "index.html")
+
+    else:
+        student = None
+        return render(request, "index.html")
