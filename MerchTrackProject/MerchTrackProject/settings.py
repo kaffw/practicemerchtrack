6d1124278f49653e903aac3badf9dc9aa2d36aa8
kaffw <coroneljm01@gmail.com>
2024-05-12 18:45:57 +0800

    ADD
    re-usable navigator bar

--- MerchTrackProject/MerchTrackProject/settings.py
+++ MerchTrackProject/MerchTrackProject/settings.py
@@ -120,7 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Image files
+# MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
