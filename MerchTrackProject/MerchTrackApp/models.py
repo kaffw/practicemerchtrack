30e56204ff31e457b153ec00e97ac4eed245e63f
kaffw <coroneljm01@gmail.com>
2024-05-15 01:43:37 +0800

    ADD
    Order DB

--- MerchTrackProject/MerchTrackApp/models.py
+++ MerchTrackProject/MerchTrackApp/models.py
@@ -6,3 +6,5 @@ from django.core.validators import RegexValidator
 class userInfo(models.Model):
     student_id = models.CharField(max_length=9, validators=[RegexValidator(r'^[0-9]{1,9}$', 'Student ID must be numeric and have at most 9 digits.')])
     student_name = models.CharField(max_length=200)
+
+# class order_info(models.Model):
