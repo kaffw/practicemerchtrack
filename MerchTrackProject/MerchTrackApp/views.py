a97bef251cfe4f1c9656a1602f576b3ea0a33a39
kaffw <coroneljm01@gmail.com>
2024-05-16 23:18:56 +0800

    CLEAN
    Clean up of comment for trackOrder views

--- MerchTrackProject/MerchTrackApp/views.py
+++ MerchTrackProject/MerchTrackApp/views.py
@@ -10,26 +10,17 @@ def home(request):
 def trackOrder(request):
     try:
         student_id = request.GET.get('student_id')
-        order_details_id = request.GET.get('order_details_id')
-        
+
         orders = order_info.objects.filter(user_info_ID=student_id).values()
         
-        
-        # order_id = get_by_id(orders)
-        
         order_detail = order_details.objects.filter(order_details_ID=orders[0]['id']).values()
-        # print("orders.id")
-        # print(orders[0]['id'])
-        # print("order deets")
-        # print(order_detail)
-        # order_details = order_info.objects.get(pk=orders.id)
 
         template = loader.get_template('track_order.html')
         content = {
             'orders': orders,
             'order_details': order_detail,
         }
-        print(content)
+        
         return HttpResponse(template.render(content, request))
     except userInfo.DoesNotExist:
         student = None
